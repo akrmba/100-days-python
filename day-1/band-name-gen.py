c4c2809c31dbaaed1f29b18bc27af9900e706957
akrmba <akrmba@gmail.com>
2026-03-02 12:47:43 +0000

    save

---
+++ day-1/band-name-gen.py
@@ -0,0 +1,19 @@
+#very first attempt
+# print("welcome to Band Name Generator")
+# city_grew = (input
+#              ("enter the city name that you grew\n"))
+
+# pet_name = (input
+#             ("your paw companion name\n"))
+
+# print("your band name "+city_grew+ " "+pet_name)
+
+#i couldn't continue, but learned few things here and there, this is after few weeks or may be months updated code
+print("-" * 45)
+print("welcome to brand name generator")
+print("-" * 45)
+city = input("whats the name of city you grew up in?\n").upper().strip()
+pet = input("what your pet name?\n").upper().strip()
+print("=" * 45)
+print(f"your band name is '{city} {pet}'")
+print("=" * 45)
